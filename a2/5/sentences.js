f469050e4a10269110f021f673513d66cbdb9c57
smarterGerman <65780492+smarterGerman@users.noreply.github.com>
2023-11-07 09:43:17 +0100

    Update sentences.js

--- a2/5/sentences.js
+++ a2/5/sentences.js
@@ -2,7 +2,7 @@ function easyGame() {
  difficulty_level = 1;
  tool_container.classList.remove("hide-konstantin");
  tmp_content = ["Liebe Maria,",
-"Entschuldige bitte,",
+"entschuldige bitte,",
 "aber ich verspäte mich ein paar Minuten,",
 "weil ich die Bahn verpasst habe.",
 "Ich bin in zehn Minuten bei dir.",
@@ -17,7 +17,7 @@ function normalGame() {
  difficulty_level = 2;
  tool_container.classList.remove("hide-konstantin");
  tmp_content = ["Liebe Maria,",
-"Entschuldige bitte,",
+"entschuldige bitte,",
 "aber ich verspäte",
 "mich ein paar",
 "Minuten,",
@@ -38,7 +38,7 @@ function hardGame() {
  difficulty_level = 3;
  tool_container.classList.remove("hide-konstantin");
  tmp_content = ["Liebe Maria,",
-"Entschuldige bitte,",
+"entschuldige bitte,",
 "aber ich verspäte mich ein paar Minuten,",
 "weil ich die Bahn verpasst habe.",
 "Ich bin in zehn Minuten bei dir.",
