0fd166d32270ee0a491eadd7854379c1206c058d
Michael Schmitz <65780492+smarterGerman@users.noreply.github.com>
2025-10-07 23:09:17 +0200

    Fix punctuation in sentences.js

--- a2/17/sentences.js
+++ a2/17/sentences.js
@@ -3,7 +3,7 @@ function easyGame() {
  tool_container.classList.remove("hide-konstantin");
  tmp_content = ["Liebe Liese,",
 "dein Mann hat bald Geburtstag",
-"und ich würde ihm gerne etwas schenken,",
+"und ich würde ihm gerne etwas schenken.",
 "Worüber würde er sich freuen?",
 "Vielleicht hast du ja eine Idee.",
 "Ich freu mich, von dir zu hören.",
@@ -21,7 +21,7 @@ function normalGame() {
 "Geburtstag",
 "und ich würde",
 "ihm gerne etwas",
-"schenken,",
+"schenken.",
 "Worüber würde",
 "er sich",
 "freuen?",
@@ -40,7 +40,7 @@ function hardGame() {
  tool_container.classList.remove("hide-konstantin");
  tmp_content = ["Liebe Liese,",
 "dein Mann hat bald Geburtstag",
-"und ich würde ihm gerne etwas schenken,",
+"und ich würde ihm gerne etwas schenken.",
 "Worüber würde er sich freuen?",
 "Vielleicht hast du ja eine Idee.",
 "Ich freu mich, von dir zu hören.",
